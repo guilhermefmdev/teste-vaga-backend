8c2d52c7d1ccaf9fa69c3a1d672c9ccb5a078b3e
Guilherme Moraes <guiferreiramoraes@outlook.com>
2023-08-06 00:39:50 -0300

    feat: products management

---
+++ src/controllers/products.js
@@ -0,0 +1,50 @@
+const ProductsModel = require('../models/products')
+
+async function get(req, res) { 
+    const Products = await ProductsModel.find()
+    if (Products == "") {
+        res.send({ msg: 'Não há produtos cadastrados!' }) 
+    } else {
+        res.send(Products)
+    }
+}
+
+async function post(req, res) {
+    const {
+        name,
+        price
+    } = req.body
+
+    const Product = new ProductsModel({
+        name,
+        price
+    })
+    
+    const productExists = await ProductsModel.findOne({ name: name })
+
+    if(productExists){
+        res.send({ msg: 'Já há um produto registrado com esse nome.'})
+    } else {
+        Product.save()
+        res.send({
+            msg: 'Produto registrado com sucesso!'
+        })
+    }
+
+
+}
+
+async function remove(req, res) {
+    const { id } = req.params
+    const remove = await ProductsModel.deleteOne({ _id: id })
+    const msg = remove.deletedCount == 1 ? 'success' : 'error'
+    res.send({
+        msg
+    })
+}
+
+module.exports = {
+    get,
+    post,
+    remove,
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
+
 const CostumersController = require('../controllers/costumers')
+const ProductsController = require('../controllers/products')
 
 router.get('/costumers', CostumersController.get)
 router.post('/costumers', CostumersController.post)
 router.delete('/costumers/:id', CostumersController.remove)
 
+router.get('/products', ProductsController.get)
+router.post('/products', ProductsController.post)
+router.delete('/products/:id', ProductsController.remove)
 
 module.exports = router
